d87bf2bea05611190275c3c01885ec010cb57f66
Joao Moreira <153958423+joaoelian204@users.noreply.github.com>
2024-12-17 02:13:36 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,11 +2,24 @@
 # Exit on error
 set -o errexit
 
-# Modify this line as needed for your package manager (pip, poetry, etc.)
+# Install dependencies
 pip install -r requirements.txt
 
-# Convert static asset files
+# Collect static files
 python manage.py collectstatic --no-input
 
-# Apply any outstanding database migrations
+# Apply database migrations
 python manage.py migrate
+
+# Create superuser if it doesn't already exist
+echo "from django.contrib.auth import get_user_model; \
+User = get_user_model(); \
+username = 'admin_django'; \
+email = 'admin@example.com'; \
+password = 'admin123'; \
+if not User.objects.filter(username=username).exists(): \
+    User.objects.create_superuser(username, email, password); \
+    print('Superuser created successfully.'); \
+else: \
+    print('Superuser already exists.');" | python manage.py shell
+
