cb9cd8d55bd8931c6dfbf2d75d3d6face124a16c
Joao Moreira <153958423+joaoelian204@users.noreply.github.com>
2024-12-19 17:13:46 -0500

    Update urls.py

--- apps/dueño/urls.py
+++ apps/dueño/urls.py
@@ -6,7 +6,6 @@ urlpatterns = [
     path('dashboard/', views.dashboard, name='dashboard'),
     path('gestion-reservas/', views.gestion_reservas, name='gestion_reservas'),
     path('clientes/', views.gestionar_clientes, name='gestionar_clientes'),
-    path('configuraciones/', views.configuraciones_generales, name='configuraciones_generales'),
 ]
 
 
