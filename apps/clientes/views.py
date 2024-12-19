0a70f0593bbdb39a33f6bb91c506d8d72a69b9b5
Joao Moreira <153958423+joaoelian204@users.noreply.github.com>
2024-12-19 17:00:38 -0500

    Update views.py

--- apps/clientes/views.py
+++ apps/clientes/views.py
@@ -1,6 +1,7 @@
 from django.contrib import messages
 from django.contrib.auth import update_session_auth_hash
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404, redirect, render
 
 from apps.clientes.models import Cliente
